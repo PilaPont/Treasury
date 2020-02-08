ba05e127143c1e472d93ba9d4402118b787b4372
Maryam Kia <maryam.kia1984@mail.com>
2020-02-09 03:05:23 +0330

    treasury module, WIP revise action_bounce function incoming model

--- models/treasury_incoming.py
+++ models/treasury_incoming.py
@@ -140,25 +140,27 @@ class TreasuryIncoming(models.Model):
     @api.multi
     def action_bounce(self):
         self.state = 'bounced'
-        if not self.guaranty:
-            debit_line_vals = {
-                'name': self.name,
-                'debit': self.amount,
-                'account_id': self.company_id.incoming_securities_account_id.id,
-            }
-            credit_line_vals = {
-                'name': self.name,
-                'credit': self.amount,
-                'account_id': self.company_id.incoming_securities_in_bank_account_id.id,
-            }
 
-            vals = {
-                'journal_id': self.company_id.treasury_journal_id.id,
-                'partner_id': self.consignee_id.id,
-                'line_ids': [(0, 0, debit_line_vals), (0, 0, credit_line_vals)],
-                'ref': 'bounce security'
-            }
-            return self.env['account.move'].create(vals).id
+        debit_line_vals = {
+            'name': 'Bouncing {} {} for {}'.format(self.security_type_id.name, self.number,
+                                                   self.env.context.get('payment_description') or self.reason),
+            'debit': self.amount,
+            'account_id': self.consignee_id.property_account_receivable_id.id,
+        }
+        credit_line_vals = {
+            'name': self.name,
+            'credit': self.amount,
+            'account_id': self.company_id.incoming_securities_in_bank_account_id.id,
+        }
+
+        vals = {
+            'journal_id': self.company_id.treasury_journal_id.id,
+            'partner_id': self.consignee_id.id,
+            'line_ids': [(0, 0, debit_line_vals), (0, 0, credit_line_vals)],
+            'ref': 'Bouncing {} {} for {}'.format(self.security_type_id.name, self.number,
+                                                  self.env.context.get('payment_description') or self.reason),
+        }
+        return self.env['account.move'].create(vals).id
 
     @api.multi
     def action_sue(self):
